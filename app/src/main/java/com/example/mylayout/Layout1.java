ee7041c05920b9c79050b81f9ada307d5a3bd254
nth6990 <56827359+nth6990@users.noreply.github.com>
2020-03-02 22:17:52 +0900

    alomst

---
+++ app/src/main/java/com/example/mylayout/Layout1.java
@@ -0,0 +1,39 @@
+package com.example.mylayout;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+public class Layout1 extends LinearLayout {
+    ImageView imageView;
+    TextView textView1;
+    TextView textView2;
+    public Layout1(Context context) {
+        super(context);
+        init(context);
+    }
+
+    public Layout1(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
+    private void init(Context context){
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.layout1, this, true);
+
+        imageView = findViewById(R.id.image);
+        textView1 = findViewById(R.id.name);
+        textView2 = findViewById(R.id.mobile);
+    }
+    public void setName(String string){
+        textView1.setText(string);
+    }
+    public void setMoblie(String moblie){
+        textView2.setText(moblie);
+    }
+}
--- app/src/main/java/com/example/mylayout/MainActivity.java
+++ app/src/main/java/com/example/mylayout/MainActivity.java
@@ -5,10 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    Layout1 layout1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        layout1 = findViewById(R.id.layout1);
+        layout1.setName("태훈");
+        layout1.setMoblie("1234");
     }
 }
